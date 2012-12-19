d74e13b5cc6e980d16d8a356f86fe801466d6b9d
Erick Pintor <erickpintor@gmail.com>
2012-12-19 09:53:07 -0200

    Adding utils file

---
+++ utils.sh
@@ -0,0 +1,15 @@
+ff() {
+	find . -type f -iname "*$1*" -not -name "*.svn*" 
+}
+
+ft() {
+	find . -type f -iname "*.$1" -not -name "*.svn*" 
+}
+
+fd() { 
+	find . -type d -iname "*$1*" -not -name "*.svn*" 
+}
+
+vv() {
+	$@ | vim -R -
+}
